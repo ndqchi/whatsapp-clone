12c0fcd6186e280d7b161549f251da2249f6528c
jolie <ndqchi1811@gmail.com>
2020-10-16 13:23:41 +0800

    Updates

---
+++ precache-manifest.770a07d7567616514848699ab488eb5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41d92283f991d9ed77db078fc4701329",
+    "url": "/whatsapp-clone/index.html"
+  },
+  {
+    "revision": "2ac27f663f5783415e27",
+    "url": "/whatsapp-clone/static/css/main.192c519b.chunk.css"
+  },
+  {
+    "revision": "d27cdc2d063517425562",
+    "url": "/whatsapp-clone/static/js/2.4029636b.chunk.js"
+  },
+  {
+    "revision": "2ac27f663f5783415e27",
+    "url": "/whatsapp-clone/static/js/main.838a7e26.chunk.js"
+  },
+  {
+    "revision": "d709f9f0cc15c7c8cf05",
+    "url": "/whatsapp-clone/static/js/runtime~main.3e72b4d8.js"
+  }
+]);
